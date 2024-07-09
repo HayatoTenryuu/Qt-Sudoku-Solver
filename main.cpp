e5b3c3c96aa70fc46672fcd19e79fbafcd74c855
Hayato Tenryuu <hayatotenryuu@gmail.com>
2024-07-09 17:11:00 -0500

    Better style control

--- main.cpp
+++ main.cpp
@@ -1,10 +1,33 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <QFile>
+#include <QTextStream>
+
+QString readTextFile(QString path)
+{
+    QFile file(path);
+
+    if (file.open(QIODevice::ReadOnly | QIODevice::Text))
+    {
+        QTextStream in(&file);
+        return in.readAll();
+    }
+
+    return "";
+}
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    QString styles = readTextFile(":/Resources/styles.css");
+
+    if(styles.length() > 0)
+    {
+        a.setStyleSheet(styles);
+    }
+
     MainWindow w;
     w.setWindowTitle("Sudoku Solver");
 
